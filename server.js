d7073485ebf26b278cc6174f52c199737c13cd9e
Glitch (ablaze-fridge) <none>
2018-05-15 20:13:21 +0000

    📇📅 Checkpoint ./public/client.js:1118247/1270 ./public/style.css:1118247/1035 ./views/index.html:1118247/1819 ./package.json:1118247/21 ./views/index.pug:1118247/224 ./server.js:1118247/278

--- server.js
+++ server.js
@@ -3,17 +3,20 @@
 
 // init project
 var express = require('express');
+var path = require('path');
 var app = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine","pug");
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render("index");
+
 });
 
 // listen for requests :)
